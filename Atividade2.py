4a3fcaa0e1080b265a47c292ef98365f98e39f36
Ana1104 <81185885+Ana1104@users.noreply.github.com>
2021-03-31 21:38:00 -0300

    Create Atividade2.py

---
+++ Atividade2.py
@@ -0,0 +1,32 @@
+soma = 0
+i = []
+def calcula_soma(i):
+    i = numeros
+for numeros in i:
+    soma += int(numeros)
+print(soma)
+
+
+i =
+numeros = i.split()
+lista = []
+
+def converte_entrada(numeros):
+    for numero in numeros:
+        lista.append(int(numero))
+    return lista
+
+lista = converte_entrada(numeros)
+print(lista)
+
+
+lista={2, 3, 5, 3}
+val2 = len(lista)
+val1 = 0
+
+def calcula_soma(numeros):
+    i = numeros
+for numeros in lista:
+    val1 += int(numeros)
+
+print(val1,',', val2)  
